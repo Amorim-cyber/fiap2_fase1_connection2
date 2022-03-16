9bc3d12dae403c4d916c7356bc82a28c1be47b1c
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2022-03-16 09:44:59 -0300

    adiciona classes service

---
+++ src/main/java/br/com/bar/service/ComandaService.java
@@ -0,0 +1,128 @@
+package br.com.bar.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.bar.dao.ComandaDAO;
+import br.com.bar.domain.Comanda;
+import br.com.bar.interfaces.ComandaInterface;
+import br.com.bar.mappers.ComandaMapper;
+
+public class ComandaService implements ComandaInterface{
+
+	@Override
+	public void save(ComandaDAO dao) {
+		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("Bar-Project");
+		EntityManager em = fabrica.createEntityManager();
+		
+		try {
+			em.persist(ComandaMapper.toEntity(dao));
+			
+			em.getTransaction().begin();
+			em.getTransaction().commit();
+		}catch(Exception e) {
+			if(em.getTransaction().isActive()) {
+				em.getTransaction().rollback();
+			}
+		}
+		
+		
+		em.close();
+		fabrica.close();
+		
+	}
+
+	@Override
+	public List<ComandaDAO> findAll() {
+		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("Bar-Project");
+		EntityManager em = fabrica.createEntityManager();
+		
+		List<Comanda> comandas = new ArrayList<Comanda>();
+		
+		try {
+			comandas = em.createQuery("select * from Comanda",Comanda.class).getResultList();
+			
+		}catch(Exception e) {
+			if(em.getTransaction().isActive()) {
+				em.getTransaction().rollback();
+			}
+		}
+		
+		
+		em.close();
+		fabrica.close();
+		return ComandaMapper.toDAO(comandas);
+	}
+
+	@Override
+	public ComandaDAO find(int id) {
+		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("Bar-Project");
+		EntityManager em = fabrica.createEntityManager();
+		
+		Comanda comanda = null;
+		
+		try {
+			comanda = em.find(Comanda.class, id);
+			
+		}catch(Exception e) {
+			if(em.getTransaction().isActive()) {
+				em.getTransaction().rollback();
+			}
+		}
+		
+		em.close();
+		fabrica.close();
+		return ComandaMapper.toDAO(comanda);
+	}
+
+	@Override
+	public void update(ComandaDAO dao) {
+		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("Bar-Project");
+		EntityManager em = fabrica.createEntityManager();
+		
+		
+		
+		try {
+			em.merge(ComandaMapper.toEntity(dao));
+			em.getTransaction().begin();
+			em.getTransaction().commit();
+		}catch(Exception e) {
+			if(em.getTransaction().isActive()) {
+				em.getTransaction().rollback();
+			}
+		}
+		
+		
+		em.close();
+		fabrica.close();
+		
+	}
+
+	@Override
+	public void delete(int id) {
+		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("Bar-Project");
+		EntityManager em = fabrica.createEntityManager();
+		
+		Comanda comanda = em.find(Comanda.class, id);
+		
+		try {
+			em.remove(comanda);
+			em.getTransaction().begin();
+			em.getTransaction().commit();
+			
+		}catch(Exception e) {
+			if(em.getTransaction().isActive()) {
+				em.getTransaction().rollback();
+			}
+		}
+		
+		em.close();
+		fabrica.close();
+		
+	}
+
+}
