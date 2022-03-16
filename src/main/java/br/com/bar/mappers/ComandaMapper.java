9b3648a73c72cefd20f65e994d3d8d85f50b7c78
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2022-03-16 09:40:10 -0300

    adiciona classes mappers

---
+++ src/main/java/br/com/bar/mappers/ComandaMapper.java
@@ -0,0 +1,36 @@
+package br.com.bar.mappers;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import br.com.bar.dao.ComandaDAO;
+import br.com.bar.domain.Comanda;
+
+public class ComandaMapper {
+	
+	public static Comanda toEntity(ComandaDAO dao) {
+		Comanda comanda = new Comanda();
+			comanda.setId(dao.getId());
+			comanda.setDataEntrada(dao.getDataEntrada());
+			comanda.setDataSaida(dao.getDataSaida());
+			comanda.setCliente(dao.getCliente());
+			comanda.setBebidas(dao.getBebidas());
+		return comanda;
+	}
+	
+	public static ComandaDAO toDAO(Comanda comanda) {
+		ComandaDAO dao = new ComandaDAO();
+			dao.setId(comanda.getId());
+			dao.setDataEntrada(comanda.getDataEntrada());
+			dao.setDataSaida(comanda.getDataSaida());
+			dao.setCliente(comanda.getCliente());
+			dao.setBebidas(comanda.getBebidas());
+		return dao;
+		
+	}
+	
+	public static List<ComandaDAO> toDAO(List<Comanda> comandas){
+		return comandas.stream().map(x->toDAO(x)).collect(Collectors.toList());
+	}
+
+}
